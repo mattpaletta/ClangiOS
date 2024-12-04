02a248f3aa93a7784945d38f8e84a12ce4f7d491
Matthew Paletta <mattpaletta@gmail.com>
2024-12-04 18:58:42 +0000

    Initial commit

---
+++ build.sh
@@ -0,0 +1,11 @@
+set -eoux pipefail
+mkdir -p build_host
+pushd build_host
+cmake -GXcode -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_PROJECTS=clang -DLLVM_TARGETS_TO_BUILD="Native" ..
+cmake --build . --target clang-tblgen --target llvm-tblgen | xcpretty
+popd
+mkdir -p build
+pushd build
+cmake -DCMAKE_TOOLCHAIN_FILE=../cmake/iOS_toolchain.cmake -DCMAKE_BUILD_TYPE=Release -GXcode -DLLVM_NATIVE_TOOL_DIR="$(pwd)/../build_host/third_party/llvm-project/llvm/Debug/bin" ..
+#xcodebuild build -scheme AppClangParse -sdk iphoneos | xcpretty
+popd
---
+++ src/AppClangParse.cpp
@@ -0,0 +1,2 @@
+#include "clang/ASTMatchers/ASTMatchers.h"
+#incldue "mlir/ExecutionEngine/ExecutionEngine.h"
