3fdc73f44d24cc389b816df8140322f81c604fc7
Matthew Paletta <mattpaletta@gmail.com>
2024-12-05 18:10:58 +0000

    Build for simulator instead

--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ popd
 mkdir -p build
 pushd build
 cmake -DCMAKE_TOOLCHAIN_FILE=../cmake/iOS_toolchain.cmake -DCMAKE_BUILD_TYPE=Release -GXcode -DLLVM_NATIVE_TOOL_DIR="$(pwd)/../build_host/third_party/llvm-project/llvm/Debug/bin" ..
-xcodebuild -target AppClangParse ARCHS=arm64 -sdk iphoneos -configuration Release | xcpretty
+xcodebuild -target AppClangParse ARCHS=arm64 -sdk iphonesimulator -configuration Release
 popd
